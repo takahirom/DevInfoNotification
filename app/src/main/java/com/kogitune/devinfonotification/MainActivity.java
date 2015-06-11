ce3518bad7c768d950146e636951cb33a9cbc2f3
takahirom <takam.dev@gmail.com>
2015-06-12 01:13:39 +0900

    add setup views method

--- app/src/main/java/com/kogitune/devinfonotification/MainActivity.java
+++ app/src/main/java/com/kogitune/devinfonotification/MainActivity.java
@@ -25,26 +25,30 @@ public class MainActivity extends ActionBarActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
 
-		findViewById(R.id.go_store).setOnClickListener(new OnClickListener(){
-			
-			public void onClick(View view){
-				final String appPackageName = getPackageName();
-				try {
-					startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
-				} catch (ActivityNotFoundException e) {
-					startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?id=" + appPackageName)));
-				}
-			}
-		});
-		findViewById(R.id.go_github).setOnClickListener(new OnClickListener(){
-
-				public void onClick(View view){
-					startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/takahirom/DevInfoNotification")));
-				}
-
-			});
-
         mDevInfoNotification = new DevInfoNotification(this, new HardwareInfo());
+        mDevInfoNotification.settingByPref();
+        setupViews();
+    }
+
+    private void setupViews() {
+        findViewById(R.id.go_store).setOnClickListener(new OnClickListener() {
+
+            public void onClick(View view) {
+                final String appPackageName = getPackageName();
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+                } catch (ActivityNotFoundException e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?id=" + appPackageName)));
+                }
+            }
+        });
+        findViewById(R.id.go_github).setOnClickListener(new OnClickListener() {
+
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/takahirom/DevInfoNotification")));
+            }
+
+        });
         boolean isNotificationShow = mDevInfoNotification.isNotificationEnabled();
 
         SwitchCompat showSwitchCompat = (SwitchCompat) findViewById(R.id.is_show_notification);
@@ -53,15 +57,14 @@ public class MainActivity extends ActionBarActivity {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 mDevInfoNotification.setNotificationEnabled(isChecked);
-				if (isChecked){
+                if (isChecked) {
                     mDevInfoNotification.show();
-                }else{
+                } else {
                     mDevInfoNotification.cancel();
                 }
             }
         });
 
-        mDevInfoNotification.settingByPref();
     }
 
 
@@ -79,7 +82,7 @@ public class MainActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         /*
-		if (id == R.id.action_settings) {
+        if (id == R.id.action_settings) {
             return true;
         }
 		*/
