1dc72bc00b67ec372abafea5c621a758d51f05b5
takahirom <takam.dev@gmail.com>
2015-11-28 20:58:24 +0900

    Add LICENSE

--- app/src/main/java/com/kogitune/devinfonotification/apps/AppsAdapter.java
+++ app/src/main/java/com/kogitune/devinfonotification/apps/AppsAdapter.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2015 takahirom
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.kogitune.devinfonotification.apps;
 
 import android.content.pm.PackageInfo;
@@ -15,9 +31,6 @@ import com.kogitune.devinfonotification.R;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by takam on 2015/06/11.
- */
 public class AppsAdapter extends RecyclerView.Adapter<AppsAdapter.ContactViewHolder> {
 
     private final PackageManager packageManager;
